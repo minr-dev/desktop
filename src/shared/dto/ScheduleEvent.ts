f8ba1d3678633f9aa462072880351b465abfcc6b
Madoka Sato <msato@altus5.com>
2023-08-02 20:39:04 +0900

    refactor: 未使用の定義を削除

--- src/shared/dto/ScheduleEvent.ts
+++ src/shared/dto/ScheduleEvent.ts
@@ -1,13 +1,11 @@
 export const EVENT_TYPE = {
   PLAN: 'PLAN',
   ACTUAL: 'ACTUAL',
-  ACTIVITY: 'ACTIVITY',
 } as const;
 export type EVENT_TYPE = (typeof EVENT_TYPE)[keyof typeof EVENT_TYPE];
 export const EVENT_TYPE_ITEMS: { id: EVENT_TYPE; name: string }[] = [
   { id: EVENT_TYPE.PLAN, name: '予定' },
   { id: EVENT_TYPE.ACTUAL, name: '実績' },
-  { id: EVENT_TYPE.ACTIVITY, name: 'アクティビティ' },
 ];
 
 export interface ScheduleEvent {
