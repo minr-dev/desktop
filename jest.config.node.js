96f7c3661e3c2853eb741cf7b9791449fa9e62a3
Madoka Sato <msato@altus5.com>
2023-08-11 15:15:54 +0900

    chore: ユニットテストをmainとrendererで実行を分ける

--- jest.config.node.js
+++ jest.config.node.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
+  testMatch: ['<rootDir>/src/main/**/*.test.ts', '<rootDir>/src/shared/**/*.test.ts'],
   globals: {
     'ts-jest': {
       diagnostics: false,
