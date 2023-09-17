ae13a20071e87b0094753fbb38fa1cab2b20912a
Madoka Sato <msato@altus5.com>
2023-09-17 09:28:38 +0900

    feat: #88 openaiのapiキーを設定項目に追加

--- src/renderer/src/components/settings/AccountSetting.tsx
+++ src/renderer/src/components/settings/AccountSetting.tsx
@@ -1,9 +1,36 @@
-import { Grid, Paper, Alert, Button, FormLabel } from '@mui/material';
+import rendererContainer from '@renderer/inversify.config';
+import { Grid, Paper, Alert, Button, FormLabel, TextField } from '@mui/material';
 import { useGoogleAuth } from '@renderer/hooks/useGoogleAuth';
 import { useGitHubAuth } from '@renderer/hooks/useGitHubAuth';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { useContext, useEffect } from 'react';
+import UserContext from '../UserContext';
+import { useUserPreference } from '@renderer/hooks/useUserPreference';
+import { UserPreference } from '@shared/data/UserPreference';
+import { useSnackbar } from 'notistack';
+import { AppError } from '@shared/errors/AppError';
+import { TYPES } from '@renderer/types';
+import { IUserPreferenceProxy } from '@renderer/services/IUserPreferenceProxy';
+import { SettingFormBox } from './SettingFormBox';
 
 export const AccountSetting = (): JSX.Element => {
   console.log('AccountSetting');
+  const { userDetails } = useContext(UserContext);
+  const { userPreference, loading } = useUserPreference();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors: formErrors },
+    reset,
+  } = useForm<UserPreference>();
+  const { enqueueSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    if (userPreference) {
+      reset(userPreference);
+    }
+  }, [reset, userPreference]);
 
   // Google 認証の hook
   const {
@@ -21,87 +48,145 @@ export const AccountSetting = (): JSX.Element => {
     handleRevoke: handleGitHubRevoke,
   } = useGitHubAuth();
 
+  // 保存ハンドラー
+  const onSubmit: SubmitHandler<UserPreference> = async (data: UserPreference): Promise<void> => {
+    console.log('AccountSetting onSubmit');
+    if (!userDetails) {
+      throw new AppError('userDetails is null');
+    }
+    if (Object.keys(formErrors).length === 0) {
+      // エラーがない場合の処理
+      console.log('フォームデータの送信:', data);
+      const userPreferenceProxy = rendererContainer.get<IUserPreferenceProxy>(
+        TYPES.UserPreferenceProxy
+      );
+      const userPreference = await userPreferenceProxy.getOrCreate(userDetails.userId);
+      const updateData = { ...userPreference, ...data };
+      await userPreferenceProxy.save(updateData);
+
+      enqueueSnackbar('保存しました。', { variant: 'info' });
+    }
+  };
+
+  // キャンセル時はフォームをリセット
+  const onCancel = (): void => {
+    if (!userPreference) {
+      throw new AppError('userPreference is null');
+    }
+    reset(userPreference);
+  };
+
+  // データがまだ読み込まれていない場合はローディングスピナーを表示
+  if (loading || !userDetails) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
-      <p>Account</p>
-      <Paper variant="outlined">
-        <Grid container spacing={2} padding={2}>
-          <Grid item xs={12}>
-            <Paper variant="outlined">
-              <Grid container spacing={2} padding={2}>
-                <Grid item>
-                  <FormLabel>Google</FormLabel>
-                </Grid>
-                <Grid item>
-                  <>
-                    {!isGoogleAuthenticated && (
-                      <>
+      <SettingFormBox onSubmit={handleSubmit(onSubmit)} onCancel={onCancel} errors={formErrors}>
+        <Paper variant="outlined">
+          <Grid container spacing={2} padding={2}>
+            <Grid item xs={12}>
+              <Paper variant="outlined">
+                <Grid container spacing={2} padding={2}>
+                  <Grid item>
+                    <FormLabel>Google</FormLabel>
+                  </Grid>
+                  <Grid item>
+                    <>
+                      {!isGoogleAuthenticated && (
+                        <>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            disabled={isGoogleAuthenticated === null}
+                            onClick={handleGoogleAuth}
+                          >
+                            認証する
+                          </Button>
+                          {/* 認証エラー */}
+                          {googleAuthError && <Alert severity="error">{googleAuthError}</Alert>}
+                        </>
+                      )}
+                      {isGoogleAuthenticated && (
                         <Button
-                          variant="contained"
-                          color="primary"
-                          disabled={isGoogleAuthenticated === null}
-                          onClick={handleGoogleAuth}
+                          variant="outlined"
+                          color="secondary"
+                          style={{ marginLeft: '1ch' }}
+                          onClick={handleGoogleRevoke}
                         >
-                          認証する
+                          認証解除
                         </Button>
-                        {/* 認証エラー */}
-                        {googleAuthError && <Alert severity="error">{googleAuthError}</Alert>}
-                      </>
-                    )}
-                    {isGoogleAuthenticated && (
-                      <Button
-                        variant="outlined"
-                        color="secondary"
-                        style={{ marginLeft: '1ch' }}
-                        onClick={handleGoogleRevoke}
-                      >
-                        認証解除
-                      </Button>
-                    )}
-                  </>
+                      )}
+                    </>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper variant="outlined">
-              <Grid container spacing={2} padding={2}>
-                <Grid item>
-                  <FormLabel>GitHub</FormLabel>
-                </Grid>
-                <Grid item>
-                  <>
-                    {!isGitHubAuthenticated && (
-                      <>
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper variant="outlined">
+                <Grid container spacing={2} padding={2}>
+                  <Grid item>
+                    <FormLabel>GitHub</FormLabel>
+                  </Grid>
+                  <Grid item>
+                    <>
+                      {!isGitHubAuthenticated && (
+                        <>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            disabled={isGitHubAuthenticated === null}
+                            onClick={handleGitHubAuth}
+                          >
+                            認証する
+                          </Button>
+                          {/* 認証エラー */}
+                          {githubAuthError && <Alert severity="error">{githubAuthError}</Alert>}
+                        </>
+                      )}
+                      {isGitHubAuthenticated && (
                         <Button
-                          variant="contained"
-                          color="primary"
-                          disabled={isGitHubAuthenticated === null}
-                          onClick={handleGitHubAuth}
+                          variant="outlined"
+                          color="secondary"
+                          style={{ marginLeft: '1ch' }}
+                          onClick={handleGitHubRevoke}
                         >
-                          認証する
+                          認証解除
                         </Button>
-                        {/* 認証エラー */}
-                        {githubAuthError && <Alert severity="error">{githubAuthError}</Alert>}
-                      </>
-                    )}
-                    {isGitHubAuthenticated && (
-                      <Button
-                        variant="outlined"
-                        color="secondary"
-                        style={{ marginLeft: '1ch' }}
-                        onClick={handleGitHubRevoke}
-                      >
-                        認証解除
-                      </Button>
-                    )}
-                  </>
+                      )}
+                    </>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </Paper>
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper variant="outlined" sx={{ padding: 2 }}>
+                <Controller
+                  name={`openAiKey`}
+                  control={control}
+                  rules={{
+                    required: '入力してください。',
+                  }}
+                  render={({ field, fieldState: { error } }): React.ReactElement => (
+                    <>
+                      <TextField
+                        label="OpenAI API KEY"
+                        {...field}
+                        error={!!error}
+                        helperText={error?.message}
+                        variant="outlined"
+                        fullWidth
+                        margin="none"
+                      />
+                    </>
+                  )}
+                />
+              </Paper>
+            </Grid>
           </Grid>
-        </Grid>
-      </Paper>
+        </Paper>
+      </SettingFormBox>
     </>
   );
 };
