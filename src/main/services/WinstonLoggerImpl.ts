e0e344f1ef81fe3f9fda85650af310e27c79523b
Yanagi <yanagi@altus5.com>
2024-10-31 19:09:01 +0900

    feat: #118 ファイルロギング
    レビュー指摘対応: 環境変数のロガーレベルを小文字に変換する処理を追加

--- src/main/services/WinstonLoggerImpl.ts
+++ src/main/services/WinstonLoggerImpl.ts
@@ -23,7 +23,7 @@ export class WinstonLoggerImpl implements ILogger {
       logFilePath = './log';
     }
     this.logger = winston.createLogger({
-      level: process.env.LOG_LEVEL || 'info',
+      level: process.env.LOG_LEVEL?.toLowerCase() || 'info',
       format: winston.format.combine(
         winston.format.timestamp({ format: 'YYYY/MM/DD HH:mm:ss Z' }),
         winston.format.printf(({ timestamp, level, processType, loggerName, message }) => {
