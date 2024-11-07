275ee65e389f8ed9d05616c0669db3031389fa7d
Yanagi <yanagi@altus5.com>
2024-11-07 17:29:37 +0900

    feat: #118 ファイルロギング
    レビュー指摘対応: inTransientScope利用についてコメントを再修正

--- src/main/inversify.logger.config.ts
+++ src/main/inversify.logger.config.ts
@@ -8,10 +8,9 @@ import { WinstonLoggerImpl } from './services/WinstonLoggerImpl';
 // コンテナの作成
 const container = new Container();
 
-// LoggerFactoryImpl の getLogger は呼び出し時に WinstonLoggerImpl の setName , setProcessType を実行しているため、
-// inSingletonScope と inRequestScope ではインスタンスを共有していることから getLogger が同時に複数モジュールで呼び出されると setName , setProcessType を
-// 同時に呼び出してしまい、 WinstonLoggerImpl でログ出力する際に name , processType が正しく出力されない不具合が発生する。
-// そのため、 inTransientScope で呼び出しごとにインスタンスを新しく作成することで setName , setProcessType が同時に呼び出されないようにする。
+// LoggerFactoryImpl の getLogger が同時に複数モジュールで呼び出されると WinstonLoggerImpl の setName を同時に呼び出してしまい、
+// インスタンスを共有していると後から呼び出された setName でその前の setName を上書きしてしまう不具合が発生する。
+// そのため、 inTransientScope で呼び出しごとにインスタンスを新しく作成することで setName が同時に呼び出されないようにする。
 container.bind<ILoggerFactory>(TYPES.LoggerFactory).to(LoggerFactoryImpl).inTransientScope();
 container.bind<ILogger>(TYPES.WinstonLogger).to(WinstonLoggerImpl).inTransientScope();
 
