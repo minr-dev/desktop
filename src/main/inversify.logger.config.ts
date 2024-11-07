194dcab1cc8a8fac190667e2b8ca7474ae2459d7
Yanagi <yanagi@altus5.com>
2024-11-07 10:05:35 +0900

    feat: #118 ファイルロギング
    レビュー指摘対応: inTransientScope利用についてコメントを追加

--- src/main/inversify.logger.config.ts
+++ src/main/inversify.logger.config.ts
@@ -8,7 +8,8 @@ import { WinstonLoggerImpl } from './services/WinstonLoggerImpl';
 // コンテナの作成
 const container = new Container();
 
-// ロガーのバインド
+// inSingletonScope, inRequestScope を使用するとライフサイクルの問題でログ出力の上書きが発生する。
+// inTransientScope を使用することで呼び出されるたびに新しいインスタンスを作成し上書きが発生しないようにする。
 container.bind<ILoggerFactory>(TYPES.LoggerFactory).to(LoggerFactoryImpl).inTransientScope();
 container.bind<ILogger>(TYPES.WinstonLogger).to(WinstonLoggerImpl).inTransientScope();
 
