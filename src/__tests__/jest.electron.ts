e18ba67e7d8962b39ae76f4ff3463849ecee27c4
Yanagi <yanagi@altus5.com>
2024-11-20 14:32:57 +0900

    fix: #188 テスト時にloggerでgetPathを呼び出すとエラーになる
    レビュー指摘対応: electronのモック化処理にコメントを追加
    lint対応

--- src/__tests__/jest.electron.ts
+++ src/__tests__/jest.electron.ts
@@ -1,5 +1,7 @@
-import path from "path";
+import path from 'path';
 
+// テスト実行時にelectronのライブラリを使用すると認識されずエラーになってしまうため、モック化する。
+// * electronのメソッドを新たにモック化する場合はappに追加する。
 export const app = {
   getPath: jest.fn().mockReturnValue(path.join(process.cwd(), '.test')),
 };
