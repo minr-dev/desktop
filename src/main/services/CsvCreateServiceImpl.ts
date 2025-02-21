2ad24d73d80c852e8ea9c0bfff18b91019d047c6
Yanagi <yanagi@altus5.com>
2025-02-21 15:13:08 +0900

    feat: #159 予実管理をCSVに出力する
    レビュー指摘対応:convertArrayToStringに説明文を追加

--- src/main/services/CsvCreateServiceImpl.ts
+++ src/main/services/CsvCreateServiceImpl.ts
@@ -30,10 +30,21 @@ export class CsvCreateServiceImpl<T> implements ICsvCreateService<T> {
     });
   }
 
-  convertArrayToString(datas: string[]): string {
+  /**
+   * CSV作成で使用する配列を文字列に変換します。
+   * 
+   * memo:
+   * csv-stringifyのstringifyで配列の変換をしているのは、
+   * 同じライブラリを使用することでエスケープ処理の差異を心配する必要が無い
+   * という点からこのような実装になっています。
+   * 
+   * @param array 
+   * @returns 文字列に変換された配列
+   */
+  convertArrayToString(array: string[]): string {
     // stringify で配列を引数にするには2次元配列である必要があるため変換を行う。
-    const dataArray = [datas];
-    const output: string = syncStringify(dataArray, { header: false, eof: false });
+    const dimensionnalArray = [array];
+    const output: string = syncStringify(dimensionnalArray, { header: false, eof: false });
     return output;
   }
 }
