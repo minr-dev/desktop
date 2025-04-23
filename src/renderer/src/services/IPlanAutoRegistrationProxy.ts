4086eee2962023128c3885eb6dbbadf64daa5c26
Yanagi <yanagi@altus5.com>
2025-04-23 10:25:56 +0900

    feat: #272 予定の自動登録で、登録タスクのプロジェクトを指定できるようにする
    引き数の形式がmainと同じになるように修正

--- src/renderer/src/services/IPlanAutoRegistrationProxy.ts
+++ src/renderer/src/services/IPlanAutoRegistrationProxy.ts
@@ -1,11 +1,13 @@
 import { PlanAutoRegistrationResult } from '@shared/data/PlanAutoRegistrationResult';
 
+export interface PlanAutoRegistrationParams {
+  targetDate: Date;
+  projectId?: string;
+  taskExtraHours?: Map<string, number>;
+}
+
 export interface IPlanAutoRegistrationProxy {
-  autoRegisterProvisonal(
-    targetDate: Date,
-    taskExtraHours?: Map<string, number>,
-    projectId?: string
-  ): Promise<PlanAutoRegistrationResult>;
+  autoRegisterProvisonal(params: PlanAutoRegistrationParams): Promise<PlanAutoRegistrationResult>;
   confirmRegistration(targetDate: Date): Promise<void>;
   deleteProvisional(targetDate: Date): Promise<void>;
 }
--- src/renderer/src/services/PlanAutoRegistrationProxy.ts
+++ src/renderer/src/services/PlanAutoRegistrationProxy.ts
@@ -1,16 +1,19 @@
 import { injectable } from 'inversify';
-import { IPlanAutoRegistrationProxy } from './IPlanAutoRegistrationProxy';
+import {
+  IPlanAutoRegistrationProxy,
+  PlanAutoRegistrationParams,
+} from './IPlanAutoRegistrationProxy';
 import { handleIpcOperation } from './ipcErrorHandling';
 import { IpcChannel } from '@shared/constants';
 import { PlanAutoRegistrationResult } from '@shared/data/PlanAutoRegistrationResult';
 
 @injectable()
 export class PlanAutoRegistrationProxy implements IPlanAutoRegistrationProxy {
-  async autoRegisterProvisonal(
-    targetDate: Date,
-    taskExtraHours?: Map<string, number>,
-    projectId?: string
-  ): Promise<PlanAutoRegistrationResult> {
+  async autoRegisterProvisonal({
+    targetDate,
+    projectId,
+    taskExtraHours = new Map<string, number>(),
+  }: PlanAutoRegistrationParams): Promise<PlanAutoRegistrationResult> {
     return await handleIpcOperation(async () => {
       return await window.electron.ipcRenderer.invoke(
         IpcChannel.AUTO_REGISTER_PROVISIONAL_PLANS,
