5ece3c6f82f93308ebaf57751eecb0eae5ff5c0c
yanagi-altus5 <77946641+yanagi-altus5@users.noreply.github.com>
2025-04-25 14:54:28 +0900

    feat: #285 予定の自動登録で、登録タスクのプロジェクトを指定できるようにする(rendererプロセス)
    - 予定自動登録のダイアログを追加
    - refが原因で発生していたエラーを修正

--- src/renderer/src/services/PlanAutoRegistrationProxy.ts
+++ src/renderer/src/services/PlanAutoRegistrationProxy.ts
@@ -1,20 +1,25 @@
 import { injectable } from 'inversify';
-import { IPlanAutoRegistrationProxy } from './IPlanAutoRegistrationProxy';
+import {
+  IPlanAutoRegistrationProxy,
+  PlanAutoRegistrationParams,
+} from './IPlanAutoRegistrationProxy';
 import { handleIpcOperation } from './ipcErrorHandling';
 import { IpcChannel } from '@shared/constants';
 import { PlanAutoRegistrationResult } from '@shared/data/PlanAutoRegistrationResult';
 
 @injectable()
 export class PlanAutoRegistrationProxy implements IPlanAutoRegistrationProxy {
-  async autoRegisterProvisonal(
-    targetDate: Date,
-    taskExtraHours?: Map<string, number>
-  ): Promise<PlanAutoRegistrationResult> {
+  async autoRegisterProvisonal({
+    targetDate,
+    projectId,
+    taskExtraHours = new Map<string, number>(),
+  }: PlanAutoRegistrationParams): Promise<PlanAutoRegistrationResult> {
     return await handleIpcOperation(async () => {
       return await window.electron.ipcRenderer.invoke(
         IpcChannel.AUTO_REGISTER_PROVISIONAL_PLANS,
         targetDate,
-        taskExtraHours
+        taskExtraHours,
+        projectId
       );
     });
   }
